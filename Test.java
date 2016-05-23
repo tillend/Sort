62b6c4d41fd0eed4e15931809649aa2bc397f645
Lin <weihaolin2008@qq.com>
2016-05-23 20:58:25 +0800

    Test

--- Test.java
+++ Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 //		int[] a = {5,4,9,8,7,6,0,1,3,2};
-		String[] a = {"asdd","asfqw","cxbvf","asdf","da","rfewrop","z"};
-		RadixSort.RadixSort(a, 7);
+		String[] a = {"asdd","asfqw","cxbvf","asdf","da","rfewrop","wz"};
+		CountingRadixSort.CountingRadixSort(a, 2);
 //		BinarySort.BinarySort(a);
 		for(int i = 0; i < a.length; i++){
 			System.out.print(a[i] + " ");
